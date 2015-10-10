a6c0ad67295d5fbcb4fef92bf24beb95d7f91ab3
Rohit Lalchandani <rohitx15@berkeley.edu>
2015-10-09 22:10:44 -0700

    Set up main server

---
+++ app.js
@@ -0,0 +1,19 @@
+/* Creates a web server */
+var express = require('express'), // Creates an express instance
+    app = express(),
+    server = require('http').createServer(app), // Creates a web server
+    io = require('socket.io').listen(server); // Creates a socket.io instance
+
+server.listen(process.env.PORT || 3000); // Listening port for server
+console.log("Listening on port 3000");
+console.log("Visit localhost:3000 in your browser");
+
+/* Serve static content */
+app.use(express.static('public'));
+
+// Routing for webpages
+app.get('/', function(req, res) {
+
+  res.send("hello world!");
+
+});

5d661e93b66b9a40beb95b690607825c00cd1026
Anish Balaji <balaji.anish8@gmail.com>
2015-10-10 06:08:03 -0700

    idk lol

--- public/js/logic.js
+++ public/js/logic.js
@@ -72,7 +72,7 @@ function endTurn() {
 }
 
 function canKing(piece, location) {
-	if (piece.team == "gold" && location[1] == 0) || (piece.team == "blue" && location[1] == 11) || (piece.team == "red" && location[0] == 11) || (piece.team == "white" && location[0] == 0) {
+	if ((piece.team == "gold" && location[1] == 0) || (piece.team == "blue" && location[1] == 11) || (piece.team == "red" && location[0] == 11) || (piece.team == "white" && location[0] == 0)) {
 		return true;
 	}
 	return false;

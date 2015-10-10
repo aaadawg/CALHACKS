4c77e43037747f38d21b4b810cb6100b301e4139
Rohit Lalchandani <rohitx15@berkeley.edu>
2015-10-10 01:13:23 -0700

    Corrections and working on moving

--- app.js
+++ app.js
@@ -18,28 +18,33 @@ app.get('/', function(req, res) {
 
 });
 
+board = createBoard();
+
 /* Create a new piece object */
 function createPiece(team, position) {
 	piece = new Object();
 	piece.team = team;
-	piece.position = position;
 	piece.isKing = false;
 	return piece;
 }
 
+// COME BACK AND REMOVE POSITIONS
 function createBoard() {
-	return [[null, null, null, null, createPiece("blue", [0, 4]), null, createPiece("blue", [0, 6]), null, createPiece("blue", [0, 8]), null, createPiece("blue", [0, 10]), null, null, null,], 
-			[null, null, null, createPiece("blue", [1, 3]), null, createPiece("blue", [1, 5]), null, createPiece("blue", [1, 7]), null, createPiece("blue", [1, 9]), null, null, null, null,],
-			[null, null, null, null, createPiece("blue", [2, 4]), null, createPiece("blue", [2, 6]), null, createPiece("blue", [2, 8]), null, createPiece("blue", [2, 10]), null, null, null,],
-			[null, createPiece("black", [3, 1]), null, null, null, null, null, null, null, null, null, createPiece("white", [3, 11]), null, createPiece("white", [3, 13]),],
-			[createPiece("black", [4, 0]), null, createPiece("black", [4, 2]), null, null, null, null, null, null, null, null, null, createPiece("white", [4, 12]), null,],
-			[null, createPiece("black", [5, 1]), null, null, null, null, null, null, null, null, null, createPiece("white", [5, 11]), null, createPiece("white", [5, 13]),],
-			[createPiece("black", [6, 0]), null, createPiece("black", [6, 2]), null, null, null, null, null, null, null, null, null, createPiece("white", [6, 12]), null,],
-			[null, createPiece("black", [7, 0]), null, null, null, null, null, null, null, null, null, createPiece("white", [7, 11]), null, createPiece("white", [7, 13]),],
-			[createPiece("black", [8, 0]), null, createPiece("black", [8, 2]), null, null, null, null, null, null, null, null, null, createPiece("white", [8, 12]), null,],
-			[null, createPiece("black", [9, 0]), null, null, null, null, null, null, null, null, null, createPiece("white", [9, 11]), null, createPiece("white", [9, 13]),],
-			[createPiece("black", [10, 0]), null, createPiece("black", [10, 2]), null, null, null, null, null, null, null, null, null, createPiece("white", [10, 12]), null,],
-			[null, null, null, createPiece("gold", [11, 3]), null, createPiece("gold", [10, 5]), null, createPiece("gold", [10, 7]), null, createPiece("gold", [10, 9]), null, null, null, null,],
-			[null, null, null, null, createPiece("gold", [12, 4]), null, createPiece("gold", [12, 6]), null, createPiece("gold", [12, 8]), null, createPiece("gold", [12, 10]), null, null, null,],
-			[null, null, null, createPiece("gold", [13, 3]), null, createPiece("gold", [13, 5]), null, createPiece("gold", [13, 7]), null, createPiece("gold", [13, 9]), null, null, null, null,]];
+	return [[null, null, null, createPiece("blue", [2, 4]), null, createPiece("blue", [2, 6]), null, createPiece("blue", [2, 8]), null, createPiece("blue", [2, 10]), null, null],
+			[null, null, createPiece("blue", [1, 3]), null, createPiece("blue", [1, 5]), null, createPiece("blue", [1, 7]), null, createPiece("blue", [1, 9]), null, null, null],
+			[createPiece("black", [3, 1]), null, null, null, null, null, null, null, null, null, createPiece("white", [3, 11]), null],
+			[null, createPiece("black", [4, 2]), null, null, null, null, null, null, null, null, null, createPiece("white", [4, 12])],
+			[createPiece("black", [5, 1]), null, null, null, null, null, null, null, null, null, createPiece("white", [5, 11]), null],
+			[null, createPiece("black", [6, 2]), null, null, null, null, null, null, null, null, null, createPiece("white", [6, 12])],
+			[createPiece("black", [7, 0]), null, null, null, null, null, null, null, null, null, createPiece("white", [7, 11]), null],
+			[null, createPiece("black", [8, 2]), null, null, null, null, null, null, null, null, null, createPiece("white", [8, 12])],
+			[createPiece("black", [9, 0]), null, null, null, null, null, null, null, null, null, createPiece("white", [9, 11]), null],
+			[null, createPiece("black", [10, 2]), null, null, null, null, null, null, null, null, null, createPiece("white", [10, 12])],
+			[null, null, null, createPiece("gold", [12, 4]), null, createPiece("gold", [12, 6]), null, createPiece("gold", [12, 8]), null, createPiece("gold", [12, 10]), null, null],
+			[null, null, createPiece("gold", [11, 3]), null, createPiece("gold", [10, 5]), null, createPiece("gold", [10, 7]), null, createPiece("gold", [10, 9]), null, null, null],];
 }
+
+io.on('connection', function(socket) {
+	console.log("A user has connected");
+	io.emit('board', JSON.stringify(board));
+});
